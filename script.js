9125b393c5ea40bc121b4a67660b550862c4f0e3
serubi <serubi@live.com>
2018-11-26 13:44:47 +0100

    initial stuff

---
+++ script.js
@@ -0,0 +1,6 @@
+var lockButton = document.getElementById("lockControlButton");
+
+lockButton.addEventListener("click", function() {
+  lockButton.querySelector("i.fas").classList.toggle("fa-lock");
+  lockButton.querySelector("i.fas").classList.toggle("fa-unlock");
+});
