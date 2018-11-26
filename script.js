2a9914788eb14fc46436f755eeed4f0719ac3146
serubi <serubi@live.com>
2018-11-26 14:00:03 +0100

    (un)locked styles

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ var lockButton = document.getElementById("lockControlButton");
 lockButton.addEventListener("click", function() {
   lockButton.querySelector("i.fas").classList.toggle("fa-lock");
   lockButton.querySelector("i.fas").classList.toggle("fa-unlock");
+  lockButton.classList.toggle("unlocked");
 });
