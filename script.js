bd8e3cc25259f8acd7dba7824272c0053b26cf6e
serubi <serubi@live.com>
2018-11-27 09:55:52 +0100

    Lock page

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var lockButton = document.getElementById("lockControlButton");
+var lockIcon = document.getElementById("lockStatus");
 
 lockButton.addEventListener("click", function() {
   lockButton.querySelector("i.fas").classList.toggle("fa-lock");
   lockButton.querySelector("i.fas").classList.toggle("fa-unlock");
   lockButton.classList.toggle("unlocked");
+
+  lockStatus.setAttribute('data-status', lockStatus.getAttribute("data-status") == "locked" ? "unlocked" : "locked");
+
 });
